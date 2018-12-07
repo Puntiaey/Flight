311da3349ca51669d850f41f73dc2743306dacd1
Puntiaey <35341443+Puntiaey@users.noreply.github.com>
2018-12-07 16:35:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ var app = express()
 // app.use(express.static(publicDir))
 app.use('/static',express.static(path.join(__dirname,'public')))
 
-app.get('/',function(req, res){
+app.get('/flight',function(req, res){
 	res.sendFile(path.join(__dirname, 'flight.html'))
+	console.log('Search Flight')
 })
 app.get('/Template-Display-Flights.html',function(req,res){
-	res.sendFile(path.join(__dirname, 'Template-Display-Flights.html'))	
+	res.sendFile(path.join(__dirname, 'Template-Display-Flights.html'))
+	console.log('Show Flight')
 })
-
+app.get('/Passenger.html',function(req,res){
+	res.sendFile(path.join(__dirname, 'Passenger.html'))
+	console.log('Passenger')
+})
+app.get('/Insurance.html',function(req,res){
+	res.sendFile(path.join(__dirname, 'Insurance.html'))
+	console.log('Insurance')	
+})
 app.listen(3000, function(){
 	console.log('Server started at Port 3000')
 })
